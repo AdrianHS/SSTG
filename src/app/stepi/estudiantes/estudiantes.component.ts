54c3a173d0376dd1d8123bff6678b3518e54c39c
Unknown <josdead@estudiantec.cr>
2018-05-09 18:56:15 -0600

    mas cambios

--- src/app/stepi/estudiantes/estudiantes.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ngx-estudiantes',
-    template: ` <router-outlet></router-outlet>`,
-})
-export class EstudiantesComponent {
-}
